9e3fe7bce8422f5c11be80b78daa9965213b03a9
Jennifer Beck <jjbeck@yahoo-inc.com>
2016-11-16 14:37:48 -0800

    Detail has menu and List better menu.

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -3,6 +3,7 @@ package com.codepath.android.booksearch.activities;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -24,6 +25,8 @@ public class BookDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+        setupToolbar();
+
         // Fetch views
         ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
         tvTitle = (TextView) findViewById(R.id.tvTitle);
@@ -42,6 +45,13 @@ public class BookDetailActivity extends AppCompatActivity {
         Picasso.with(this).load(Uri.parse(theBook.getCoverUrl())).into(ivBookCover);
     }
 
+    private void setupToolbar() {
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
